d790a47a9f5cd3ad16bf035b4d1f5d852171ab8c
Pedro Jorge <pedro.simao10@ua.pt>
2022-05-20 19:50:06 +0100

    gaze tracking

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { AppComponent } from './app.component';
+import { GazeTrackingComponent } from './gaze-tracking/gaze-tracking.component';
 
 const routes: Routes = [
   { path: 'main-screen', component: MainScreenComponent },
+  { path: 'gaze-tracking', component: GazeTrackingComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { WebcamModule} from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GazeTrackingComponent } from './gaze-tracking/gaze-tracking.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainScreenComponent
+    MainScreenComponent,
+    GazeTrackingComponent
   ],
   imports: [
     WebcamModule,
     BrowserModule,
     AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
