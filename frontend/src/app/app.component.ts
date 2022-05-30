bc896fd409480a4866916a693cbb1b1c66afd8a7
Jerzy Andrzejewski <jerzy.andrzejewski00@gmail.com>
2022-05-30 16:51:35 +0100

    image from backend encoded

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+
+export class AppComponent implements OnInit{
+  isImageSaved: boolean = false;
+  imageUrl: string = "";
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get<any>('http://127.0.0.1:5000/movement_detection').subscribe(data => {
+      let imageBinary = data.data; //image binary data response from api
+      let imageBase64String= btoa(imageBinary);
+      this.imageUrl = 'data:image/jpeg;base64,' + imageBase64String;
+    console.log(this.imageUrl)})
+  }
 }
+
