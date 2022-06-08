0c92293f5ea9b9117ceadaa5fa20edecbc19de56
Tiago <bran.costa@ua.pt>
2022-06-08 18:00:23 +0100

    sound part running, but not tested

--- backend/Main.py
+++ backend/Main.py
@@ -3,26 +3,21 @@ from os.path import exists
 from MovementDetection import MovementDetection
 from ColorDetection import ColorDetection
 from eye_tracking import EyeTracking
+
 import numpy as np
 import io
 import base64
+import threading
+import pygame
 
-
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+api = CORS(app)
 api = Api(app)
 
-
-#vid = None
-#ret = None
-#imgB = None
-#imgA = None
-#global vid
-#global ret
-#global imgA
-#global imgB
 def image():
     global imgA
     global imgB
@@ -31,7 +26,17 @@ def image():
     ret,imgA = vid.read()
     return imgA
     
+def play_sound(name):
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load("./MIDI_files/" + name + ".midi")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        clock.tick(30) # check if playback has finished
+    
 def prepareResult(frame, grid_size, coords):
+    
+    global square_threads
+    square_threads = {x:y for (x, y) in square_threads.items() if y.is_alive()}
     row = 0
     column = 0
     if grid_size == 4:
@@ -79,13 +84,41 @@ def prepareResult(frame, grid_size, coords):
         x = int(coords[0])
         y = int(coords[1])
         frame = cv2.circle(frame, (x, y), radius=5, color=(255, 0, 0), thickness=-1)
-    
+        
+        Xs = ceil(squareX/dx) + 1
+        Ys = ceil(squareY/dy) + 1
+        
+        square = Ys * 4 + Xs
+        global squares
+        filename = squares[str(square)]
+        
+        if filename != None and square_threads[str(square)] == None:
+            
+            square_threads[str(square)] = threading.Thread(target=play_sound, args=(filename)).start()
+        
     with open("file.jpg", "wb") as dest:
         jpg = cv2.imencode(".jpg", frame)[1]
         dest.write(jpg)
     image = open("file.jpg", "rb")
     return {"data": str(base64.b64encode(image.read()))}
         
+def reload_squares():
+    global squares
+    f = open("save.txt","r")
+    for i in range(16):
+        midi = f.readline().strip()
+        if midi != "":
+            squares[str(i+1)] = f.readline().strip()
+    
+def save_squares():
+    global squares
+    f = open("save.txt","w")
+    for i in range(16):
+        if squares[str(i+1)] != None:
+            f.write(squares[str(i+1)] + "\n")
+        else:
+            f.write("\n")
+            
 class Color_Detection(Resource):
 
     def get(self):
@@ -119,17 +152,48 @@ class Eye_Tracking(Resource):
         result = prepareResult(frame, rect, coords)
         return result
         
+class Receive_File(Resource):
+    def post(self):
+        args = request.args
+        midifile = args["file"]
+        name = args["name"]
+        midifile = bin(base64.b64decode(midifile))
+        writeout = open("./" + name + ".midi", "wb")
+        writeout.write(midifile)
+        return '{}', 200
+        
+class List_Files(Resource):
+    def get(self):
+        list_of_files = os.listdir('./MIDI_files')
+        list_of_files = [x.replace(".midi", "") for x in list_of_files]
+        return {"audio": list_of_files}
+        
+class Associate_File(Resource):
+    def post(self):
+        args = request.args
+        rect = args["rectangle"]
+        name = args["sound"]
+        squares[rect] = name
+        save_squares()
+        return '{}', 200
 
 api.add_resource(Color_Detection,"/color_detection")
 api.add_resource(Movement_Detection,"/movement_detection")
 api.add_resource(Eye_Tracking,"/eye_tracking")
-
+api.add_resource(Receive_File, "/file")
+api.add_resource(List_Files, "/list")
+api.add_resource(Associate_File, "/square")
 
 if __name__ == "__main__":
     vid = cv2.VideoCapture(0)
     ret,  imgA = vid.read()
     ret,  imgB = vid.read()
-
+    
+    squares = {}
+    reload_squares()
+    
+    square_threads = {}
+    
     app.run(debug=False)
 
     vid.release()
