fcb6b876207ab812c4d1d0b2335251833dd7a235
Ricardo Ferreira <ricardoserranoferreira@ua.pt>
2022-06-13 18:18:14 +0100

    Possible color detection fix

--- backend/ColorDetection.py
+++ backend/ColorDetection.py
@@ -22,18 +22,18 @@ def ColorDetection(image,r,b,g,threshold):
     # find the colors within the specified boundaries and apply
     # the mask
     mask = cv2.inRange(image, lower, upper)
-    output = cv2.bitwise_and(image, image, mask = mask)
+    #output = cv2.bitwise_and(image, image, mask = mask)
     morph = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5), (2,2)) 
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, morph)
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,
     	cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
     positions = {"hor":None, "ver":None}
-    maxArea = 6000
+    maxArea = 500
 
     for c in cnts:
-        if cv2.contourArea(c) < maxArea:
+        if cv2.contourArea(c) > maxArea:
             (x, y, w, h) = cv2.boundingRect(c)
             positions["hor"] = x + w / 2
             positions["ver"] = y + h / 2
@@ -41,4 +41,7 @@ def ColorDetection(image,r,b,g,threshold):
 
         #cv2.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
     # show the images
+    #with open("test.jpg", "wb") as dest:
+    #    jpg = cv2.imencode(".jpg", mask)[1]
+    #    dest.write(jpg)
     return image, (positions["hor"], positions["ver"]) #np.hstack([image, output])
