5636687f2f9edd555c58b0ee42abb40a97e99bc6
Ricardo Ferreira <ricardoserranoferreira@ua.pt>
2022-07-21 14:08:04 +0100

    Put everything togerther Mediapipe hands & small time improvements on cmotion detection, small light related issue on eye tracking(should work better now)

--- backend/MovementDetection.py
+++ backend/MovementDetection.py
@@ -9,10 +9,8 @@ def MovementDetection(imageA, imageB):
     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
     # compute the Structural Similarity Index (SSIM) between the two
     # images, ensuring that the difference image is returned
-    #(score, diff) = cv2.compase_ssim(grayA, grayB, full=True)
     diff = cv2.absdiff(grayA, grayB)
 
-    #diff = (diff * 255).astype("uint8")
 
     # threshold the difference image, followed by finding contours to
     # obtain the regions of the two input images that differ
@@ -21,13 +19,8 @@ def MovementDetection(imageA, imageB):
     morph = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5), (2,2)) 
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, morph)
 
-
-
-
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     	cv2.CHAIN_APPROX_SIMPLE)
-
-
 
     cnts = imutils.grab_contours(cnts)
 
@@ -39,8 +32,6 @@ def MovementDetection(imageA, imageB):
             positions["hor"] = x + w / 2
             positions["ver"] = y + h / 2
             maxArea = cv2.contourArea(c)
-
-        # cv2.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
     # show the images
 
     if "hor" in positions.keys() and "ver" in positions.keys():
--- backend/eye_tracking.py
+++ backend/eye_tracking.py
@@ -8,10 +8,7 @@ import cv2
 from gaze_tracking import GazeTracking
 
 def EyeTracking(frame,gaze, gazeBounds):
-    #gaze = GazeTracking()
-
-
-    # We send this frame to GazeTracking to analyze it
+    #send frame to gaze module
     gaze.refresh(frame)
     verticalR = gaze.vertical_ratio()
     horizontalR = gaze.horizontal_ratio()
@@ -34,20 +31,3 @@ def EyeTracking(frame,gaze, gazeBounds):
 
 
 
-    #cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-
-    #left_pupil = gaze.pupil_left_coords()
-    #right_pupil = gaze.pupil_right_coords()
-    # cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    # cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-
-    #cv2.putText(frame, "Vertical Ratio:  " + str(verticalR) , (90, 165),               cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1, )
-    #cv2.putText(frame, "Horizontal Ratio:  " + str(horizontalR) , (90, 200), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1, )
-
-    #cv2.imshow("Demo", frame)
-
-    #if cv2.waitKey(1) == ord('q'):
-    #    break
-
-#webcam.release()
-#cv2.destroyAllWindows()
